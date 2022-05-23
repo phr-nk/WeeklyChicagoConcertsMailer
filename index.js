29b367f9f3afe252e83c166773ff68002d1fc9f1
Phrank Lenoci <fcl1118@live.com>
2022-05-23 00:41:43 -0500

    updated to use express server

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const mailer = require("./cron-mail");
+var express = require("express"),
+  app = express(),
+  port = process.env.PORT || 3000,
+  bodyParser = require("body-parser");
 
-mailer.startMailer();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.send("CRON Job server for weekly Chicago concert emails.");
+});
+
+app.listen(port, () => {
+  console.log(`Weekly Chicago Concerts Mailer listening on port ${port}`);
+  mailer.startMailer();
+});
+
 //mailer.fetchData();
 //mailer.runMailchimp();
