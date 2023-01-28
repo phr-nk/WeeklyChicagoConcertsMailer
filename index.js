2bea56d5a0ad22678e886c5a62755f58ab4476ad
Phrank Lenoci <fcl1118@live.com>
2023-01-28 16:48:05 -0600

    added function to auto create concerts

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const mailer = require("./cron-mail");
+const Sequelize = require("sequelize-cockroachdb");
+const scrapeData = require("./createConcerts");
+require("dotenv").config();
 /*
 var express = require("express"),
   app = express(),
@@ -17,6 +20,85 @@ app.listen(port, () => {
 });
 */
 
-mailer.startMailer();
+//mailer.startMailer();
+
+// Connect to CockroachDB through Sequelize
+var sequelize = new Sequelize({
+  dialect: "postgres",
+  username: "frank",
+  password: process.env.COCKROACHDB_PASSWORD,
+  host: "free-tier4.aws-us-west-2.cockroachlabs.cloud",
+  port: 26257,
+  database: process.env.COCKROACHDB_DATABASE,
+  dialectOptions: {
+    ssl: {
+      //For secure connection:
+      ca: fs.readFileSync("/etc/secrets//root.crt").toString(),
+    },
+  },
+  logging: false,
+});
+
+// Define the Concert model for the "concerts" table.
+const Concert = sequelize.define("concerts", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+  },
+  date: {
+    type: Sequelize.STRING,
+  },
+  dayOfWeek: {
+    type: Sequelize.STRING,
+  },
+  venue: {
+    type: Sequelize.STRING,
+  },
+  image: {
+    type: Sequelize.STRING,
+  },
+  link: {
+    type: Sequelize.STRING,
+  },
+  time: {
+    type: Sequelize.STRING,
+  },
+  ticketPrice: {
+    type: Sequelize.STRING,
+  },
+  genres: {
+    type: Sequelize.STRING,
+  },
+});
+
+var data = scrapeData().then((res) => {
+  Concert.sync({
+    force: true,
+  })
+    .then(function () {
+      return Concert.bulkCreate(res, { ignoreDuplicates: true });
+    })
+    .then(function () {
+      // Retrieve concerts.
+      return Concert.findAll();
+    })
+    .then(function (artists) {
+      // Print out the balances.
+      artists.forEach(function (artist) {
+        console.log(
+          artist.id + " " + artist.name + " " + artist.time + " " + artist.venue
+        );
+      });
+      process.exit(0);
+    })
+    .catch(function (err) {
+      console.error("error: " + err.message);
+      process.exit(1);
+    });
+});
+
 //mailer.fetchData();
 //mailer.runMailchimp();
