22a69e939d2386a9446cd9a80056a44d108a1119
Phrank Lenoci <fcl1118@live.com>
2023-01-28 18:40:14 -0600

    added bash scripts

---
+++ render-start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# be sure to add Chromes location to the PATH as part of your Start Command
+export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome/"
+
+npm run start
